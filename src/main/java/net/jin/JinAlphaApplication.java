7e6cf3837008fcfd5c1d94984549ae84a7410398
nice0001-angelo <nice0001@gmail.com>
2019-12-27 23:50:12 -0500

    Initial Jin-Alpha QnA with SpringBoot

---
+++ src/main/java/net/jin/JinAlphaApplication.java
@@ -0,0 +1,13 @@
+package net.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinAlphaApplication.class, args);
+	}
+
+}
