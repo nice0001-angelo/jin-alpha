f440fbc2a1dfa75958ae8316f246ac4754d9918d
nice0001-angelo <nice0001@gmail.com>
2019-12-29 21:55:42 -0500

    model value transfer to html

--- src/main/java/net/jin/web/WelcomeController.java
+++ src/main/java/net/jin/web/WelcomeController.java
@@ -1,12 +1,15 @@
 package net.jin.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 	@GetMapping("/hello")
-	public String welcome() {
+	public String welcome(String name, Model model) {
+		System.out.println("Name :" + name);
+		model.addAttribute("name", name);
 		return "welcome";
 	}
 }
