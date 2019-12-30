c6024eb568e426bfa6aac2177518bb12041d9f8b
nice0001-angelo <nice0001@gmail.com>
2019-12-29 21:58:59 -0500

    valuable add

--- src/main/java/net/jin/web/WelcomeController.java
+++ src/main/java/net/jin/web/WelcomeController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 	@GetMapping("/hello")
-	public String welcome(String name, Model model) {
+	public String welcome(String name, int age, Model model) {
 		System.out.println("Name :" + name);
 		model.addAttribute("name", name);
+		model.addAttribute("age", age);
 		return "welcome";
 	}
 }
