dbbb8a8b42256eea30d68f6a08b67eab8ec20739
nice0001-angelo <nice0001@gmail.com>
2019-12-29 22:57:04 -0500

    make UserController

---
+++ src/main/java/net/jin/web/UserController.java
@@ -0,0 +1,12 @@
+package net.jin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	@GetMapping("/create")
+	public String create() {
+		return "index";
+	}
+}
