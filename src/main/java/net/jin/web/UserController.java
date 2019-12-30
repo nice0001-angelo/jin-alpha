d66831773ecb9d0216ebf15a8862caf77e968cca
nice0001-angelo <nice0001@gmail.com>
2019-12-30 03:13:30 -0500

    UserControll add class

--- src/main/java/net/jin/web/UserController.java
+++ src/main/java/net/jin/web/UserController.java
@@ -1,13 +1,26 @@
 package net.jin.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+	
+	private List<User> users = new ArrayList<User>();
+	
 	@PostMapping("/create")
 	public String create(User user) {
 		System.out.println("User: " + user);
+		users.add(user);
 		return "index";
+	}
+	
+	@GetMapping("/list")
+	public String list() {
+		return "list";
 	}
 }
