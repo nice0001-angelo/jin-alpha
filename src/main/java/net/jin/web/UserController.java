57d9078d77f7a66a53913864d1ae587213953cb2
nice0001-angelo <nice0001@gmail.com>
2019-12-31 04:50:24 -0500

    H2 DB  setting and SPA data transfer

--- src/main/java/net/jin/web/UserController.java
+++ src/main/java/net/jin/web/UserController.java
@@ -3,20 +3,28 @@ package net.jin.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import net.jin.domain.User;
+import net.jin.domain.UserRepository;
 
 @Controller
 public class UserController {
 	
 	private List<User> users = new ArrayList<User>();
 	
+	@Autowired
+	private UserRepository userRepository; //UserRepository는 스프링부트에서 알아서 생성해줌
+	
 	@PostMapping("/create")
 	public String create(User user) {
 		System.out.println("User: " + user);
-		users.add(user);
+		//users.add(user);
+		userRepository.save(user);
 		return "redirect:/list";
 	}
 	
