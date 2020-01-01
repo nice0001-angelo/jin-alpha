76445c99610c58656b7985b6938d2a55336e3ec1
nice0001-angelo <nice0001@gmail.com>
2020-01-01 02:43:35 -0500

    @RequestMapping add

--- src/main/java/net/jin/web/UserController.java
+++ src/main/java/net/jin/web/UserController.java
@@ -1,25 +1,24 @@
 package net.jin.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.jin.domain.User;
 import net.jin.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
 
 	@Autowired
 	private UserRepository userRepository; //UserRepository는 스프링부트에서 알아서 생성해줌
 	
-	@PostMapping("/users") //not real location. just for communication
+	@PostMapping("") //not real location. just for communication
 	public String create(User user) {
 		System.out.println("User: " + user);
 		//users.add(user);
@@ -27,7 +26,7 @@ public class UserController {
 		return "redirect:/users"; //not real location. just for communication
 	}
 	
-	@GetMapping("/users") //not real location. just for communication
+	@GetMapping("") //not real location. just for communication
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list"; //real location(src/main/resources/static/user/list.html)
