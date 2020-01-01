f60faee6a0c559c5ee23da65d8360ebc2b994bd4
nice0001-angelo <nice0001@gmail.com>
2020-01-01 02:38:48 -0500

    html refactoring

--- src/main/java/net/jin/web/UserController.java
+++ src/main/java/net/jin/web/UserController.java
@@ -15,23 +15,21 @@ import net.jin.domain.UserRepository;
 @Controller
 public class UserController {
 	
-	//userRepository로 바꾸면서 필요 없어짐
-	//private List<User> users = new ArrayList<User>();
-	
+
 	@Autowired
 	private UserRepository userRepository; //UserRepository는 스프링부트에서 알아서 생성해줌
 	
-	@PostMapping("/create")
+	@PostMapping("/users") //not real location. just for communication
 	public String create(User user) {
 		System.out.println("User: " + user);
 		//users.add(user);
 		userRepository.save(user);
-		return "redirect:user/list";
+		return "redirect:/users"; //not real location. just for communication
 	}
 	
-	@GetMapping("user/list")
+	@GetMapping("/users") //not real location. just for communication
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "/user/list";
+		return "/user/list"; //real location(src/main/resources/static/user/list.html)
 	}
 }
