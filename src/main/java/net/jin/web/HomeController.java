8fbbef8ecd5b8a14faa3cd50ac705ff9549b5dbd
nice0001-angelo <nice0001@gmail.com>
2020-01-05 06:49:39 -0500

    question list with index.html

--- src/main/java/net/jin/web/HomeController.java
+++ src/main/java/net/jin/web/HomeController.java
@@ -1,13 +1,21 @@
 package net.jin.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import net.jin.domain.QuestionRepository;
 
 @Controller
 public class HomeController {
+
+	@Autowired
+	private QuestionRepository questionRepository;
 	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("questions", questionRepository.findAll()); //"questions" 라는 이름에 담아서 index.html에 전달 {{#questions}}{{/questions}} 이용
 		return "index";
 	}
 
