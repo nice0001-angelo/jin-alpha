fd8fc25e1e114838ad9190f3684e1929193d69f5
nice0001-angelo <nice0001@gmail.com>
2020-01-07 05:25:10 -0500

    question controll and show.html add

--- src/main/java/net/jin/web/QuestionController.java
+++ src/main/java/net/jin/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,4 +43,11 @@ public class QuestionController {
 		questionRepository.save(newQuestion);
 		return "redirect:/";
 	}
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		model.addAttribute("question", questionRepository.findById(id).get());
+		return "/qna/show";
+	}
+	
 }
