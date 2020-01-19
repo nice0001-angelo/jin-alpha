6766cacf0dd23b91588f662636739ffbabb50f72
nice0001-angelo <nice0001@gmail.com>
2020-01-19 07:14:34 -0500

    arramgement

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,13 +1,13 @@
-$(".answer-write input[type=submit]").click(addAnswer);
+$(".answer-write input[type=submit]").click(addAnswer); // click 시 addAnswer method call
 
 function addAnswer(e) {
 	e.preventDefault();
 	console.log("click me");
 
-	var queryString = $(".answer-write").serialize();
+	var queryString = $(".answer-write").serialize();//입력방식에 적힌 값을 쿼리 문자열로 바꿈
 	console.log("query : " + queryString);
 
-	var url = $(".answer-write").attr("action");
+	var url = $(".answer-write").attr("action"); // answer-write > action 속성 url 선택
 
 	console.log("url : " + url);
 
@@ -37,13 +37,13 @@ function onSuccess(data, status) {
 
 
 
-$("a.link-Delete-article").click(deleteAnswer);
+$("a.link-Delete-article").click(deleteAnswer); // click 시 deleteAnswer method call
 
 function deleteAnswer(e){
 	e.preventDefault();
 	
 	var deleteBtn = $(this); //ajax 안에서도 동일한 this유지하기 위해 선언
-	var url = deleteBtn.attr("href");
+	var url = deleteBtn.attr("href"); //link-Delete-article > herf 속성 url 선택
 	console.log("url : " + url);
 	
 	$.ajax({
