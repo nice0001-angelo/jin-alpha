8964f572e8e3a2ab6d9fb06190c9570db26b79c9
nice0001-angelo <nice0001@gmail.com>
2020-04-01 01:58:12 -0400

    refactoring

--- src/main/java/net/jin/web/HomeController.java
+++ src/main/java/net/jin/web/HomeController.java
@@ -41,4 +41,9 @@ public class HomeController {
 	public String board() {
 		return "/navigation/board";
 	}
+	
+	@RequestMapping("/recipes")
+	public String recipes() {
+		return "/navigation/recipes";
+	}
 }
