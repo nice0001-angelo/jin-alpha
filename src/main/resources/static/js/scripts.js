c307426248db49a623ed875dcfccd2ff4356b8aa
nice0001-angelo <nice0001@gmail.com>
2020-06-10 03:42:01 -0400

    refactoring

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -118,6 +118,34 @@ String.prototype.format = function() {
 	});
 };
 
+
+/* Raphael plugin SVG tag for World Map */
+$(document).ready(function() {
+	// Create Function
+	function randomColor() {
+		var letters = '0123456789ABCDEF'.split('');
+		var color = '#';
+		for (var i = 0; i < 6; i++)
+			color = color + letters[Math.round(Math.random()*15)];
+		return color;
+	}
+	
+	var canvas = document.getElementById('worldmap_image');
+	var paper = Raphael(canvas, 2000, 950);
+	
+	$.each(worldMapPathData, function (index, item) {
+		// Create Path
+		var path = paper.path(item['d']);
+		
+		// Stroke Path
+		path.attr('stroke', item['stroke']);
+		// Fill Path
+		if(item['fill'] != 'none') {
+				path.attr('fill', randomColor());		
+		}
+	});
+});
+
 /* Raphael plugin SVG tag for South Korean Map */
 $(document).ready(function() {
 	// Create Function
