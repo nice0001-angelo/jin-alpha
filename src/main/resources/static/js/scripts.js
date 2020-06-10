8bb65db5f04490d85679ced1d680a06cd5a0b66a
nice0001-angelo <nice0001@gmail.com>
2020-06-10 03:22:31 -0400

    update map

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -146,6 +146,33 @@ $(document).ready(function() {
 });
 
 
+/* Raphael plugin SVG tag for Canada Map */
+$(document).ready(function() {
+	// Create Function
+	function randomColor() {
+		var letters = '0123456789ABCDEF'.split('');
+		var color = '#';
+		for (var i = 0; i < 6; i++)
+			color = color + letters[Math.round(Math.random()*15)];
+		return color;
+	} 
+	
+	var canvas = document.getElementById('canadamap_image');
+	var paper = Raphael(canvas, 1000, 1000);
+	
+	$.each(canadaMapPathData, function (index, item) {
+		// Create Path
+		var path = paper.path(item['d']);
+		
+		// Stroke Path
+		path.attr('stroke', item['stroke']);
+		// Fill Path
+		if(item['fill'] != 'none') {
+				path.attr('fill', randomColor());		
+		}
+	});
+});
+
 /* Raphael plugin SVG tag for USA Map */
 $(document).ready(function() {
 	// Create Function
@@ -173,30 +200,5 @@ $(document).ready(function() {
 	});
 });
 
-/* Raphael plugin SVG tag for Canada Map */
-$(document).ready(function() {
-	// Create Function
-	function randomColor() {
-		var letters = '0123456789ABCDEF'.split('');
-		var color = '#';
-		for (var i = 0; i < 6; i++)
-			color = color + letters[Math.round(Math.random()*15)];
-		return color;
-	} 
-	
-	var canvas = document.getElementById('canadamap_image');
-	var paper = Raphael(canvas, 1000, 716);
-	
-	$.each(canadaMapPathData, function (index, item) {
-		// Create Path
-		var path = paper.path(item['d']);
-		
-		// Stroke Path
-		path.attr('stroke', item['stroke']);
-		// Fill Path
-		if(item['fill'] != 'none') {
-				path.attr('fill', randomColor());		
-		}
-	});
-});
+
 
