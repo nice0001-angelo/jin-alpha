a7abb6189e1ac917eba2e8c302900c8b43745005
nice0001-angelo <nice0001@gmail.com>
2020-06-27 16:44:51 -0400

    refactoring

--- src/main/java/net/jin/util/SecurityUtils.java
+++ src/main/java/net/jin/util/SecurityUtils.java
@@ -1,15 +1,25 @@
 package net.jin.util;
 
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SecurityUtils {
-	public String encryptSHA256(String plainText) {
-		String plainText = "";
+	public String encryptSHA256(String str) {
+		String sha = "";
 		try {
-			MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
-			sha256.update(plainText.getBytes());
-			byte byteData[] = plainText.digest();
-			
+			MessageDigest sh = MessageDigest.getInstance("SHA-256");
+			sh.update(str.getBytes());
+			byte byteData[] = sh.digest();
+			StringBuffer stringBuffer = new StringBuffer();
+			for(int i=0; i<byteData.length; i++) {
+				stringBuffer.append(Integer.toString((byteData[i]&0xff) + 0x100, 16).substring(1));
+			}
+			sha = stringBuffer.toString();
+		}catch(NoSuchAlgorithmException e) {
+			//e.printStackTrace()
+			System.out.println("Encrypt Error - NoSuchAlgorithmException");
+			sha = null;
 		}
+		return sha;
 	}
 }
