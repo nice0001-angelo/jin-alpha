87951943bc579fa35f4439a0b727b9fffd8243dc
nice0001-angelo <nice0001@gmail.com>
2020-07-08 04:21:20 -0400

    refactoring

--- src/main/java/net/jin/controller/ApiAnswerController.java
+++ src/main/java/net/jin/controller/ApiAnswerController.java
@@ -10,7 +10,6 @@ package net.jin.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
