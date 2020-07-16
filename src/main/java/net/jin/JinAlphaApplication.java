c1511ea5467035bdd5cce36bc1ab4151de47cf01
nice0001-angelo <nice0001@gmail.com>
2020-07-16 02:12:04 -0400

    Object type test

--- src/main/java/net/jin/JinAlphaApplication.java
+++ src/main/java/net/jin/JinAlphaApplication.java
@@ -7,7 +7,6 @@
  */
 package net.jin;
 
-
 import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
@@ -26,7 +25,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableSwagger2
@@ -34,24 +32,28 @@ public class JinAlphaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JinAlphaApplication.class, args);
-		
-		
 
 // null value input test	
 		System.out.println();
-		JavaHungry(null);
-		System.out.println();
 		
+		String s = null;
+		Object o = null;
+		Integer i = null;
+
+		JavaHungry(s);
+		
+		System.out.println();
+
 // 다형성 테스트(OverWatch)		
 		OverWatch ow; // 인터페이스 객체 선언
 		System.out.println("플레이할 캐릭터 번호 선택(1. 메이, 2. 리퍼, 3. 맥크리)");
 		Scanner sc = new Scanner(System.in); // 스캐너 객체
 		int n = sc.nextInt();
-		if(n==1){
+		if (n == 1) {
 			ow = new Mei(); // 업캐스팅
-		}else if(n==2){
+		} else if (n == 2) {
 			ow = new Reaper(); // 업캐스팅
-		}else{
+		} else {
 			ow = new Mccree(); // 업캐스팅
 		}
 // 선택한 조건에 따라서 부모 객체로 자식 메소드 사용(하나의 타입으로 다양한 결과를 얻어냄 / 다형성)
@@ -61,43 +63,35 @@ public class JinAlphaApplication {
 		ow.shiftButton();
 		ow.eButton();
 		ow.qButton();
-		
-		
+
 	}
-	
-	
+
 	public static void JavaHungry(String s) {
 		System.out.println("JavaHungry is String");
 	}
-	
+
 	public static void JavaHungry(Object o) {
 		System.out.println("JavaHungry is Object");
 	}
+
 	
-	public static void JavaHungry(Integer i) {
-		System.out.println("JavaHungry is Integer");
-	}
-	
+	  public static void JavaHungry(Integer i) {
+	  System.out.println("JavaHungry is Integer"); 
+	  }
+	 
+
 	@Bean
-    public Docket newsApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("Jin-Alpha")
-                .apiInfo(apiInfo())
-                .select()
-                .paths(PathSelectors.ant("/api/**"))
-                .build();
-    }
-     
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Jin Alpha API")
-                .description("Jin Alpha API")
-                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
-                .contact("Niklas Heidloff")
-                .license("Apache License Version 2.0")
-                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
-                .version("2.0")
-                .build();
-    }
+	public Docket newsApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("Jin-Alpha").apiInfo(apiInfo()).select()
+				.paths(PathSelectors.ant("/api/**")).build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Jin Alpha API").description("Jin Alpha API")
+				.termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+				.contact("Niklas Heidloff").license("Apache License Version 2.0")
+				.licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE").version("2.0")
+				.build();
+	}
 
 }
