954516f629f9de60fa009296f9eac85085b29f54
nice0001-angelo <nice0001@gmail.com>
2020-07-26 01:25:05 -0400

    refactoring

--- src/main/java/net/jin/overwatch/service/OverWatch.java
+++ src/main/java/net/jin/overwatch/service/OverWatch.java
@@ -1,10 +1,10 @@
 package net.jin.overwatch.service;
 
 public interface OverWatch {
-	void name(); // 추상 메소드
-	void lClick(); // 추상 메소드
-	void rClick(); // 추상 메소드
-	void shiftButton(); // 추상 메소드
-	void eButton(); // 추상 메소드
-	void qButton(); // 추상 메소드
+	abstract void name(); // 추상 메소드
+	abstract void lClick(); // 추상 메소드
+	abstract void rClick(); // 추상 메소드
+	abstract void shiftButton(); // 추상 메소드
+	abstract void eButton(); // 추상 메소드
+	abstract void qButton(); // 추상 메소드
 }
