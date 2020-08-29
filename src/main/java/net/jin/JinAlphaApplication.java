903ed458d07d276f617a5cc2a0d14811eb1c6038
nice0001-angelo <nice0001@gmail.com>
2020-08-29 18:24:34 -0400

    refactoring

--- src/main/java/net/jin/JinAlphaApplication.java
+++ src/main/java/net/jin/JinAlphaApplication.java
@@ -7,26 +7,16 @@ O * This is a Jin-alpha Project
  */
 package net.jin;
 
-import java.util.Scanner;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.*;
+import org.springframework.data.jpa.repository.config.*;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import net.jin.overwatch.service.OverWatch;
-import net.jin.overwatch.service.impl.Mccree;
-import net.jin.overwatch.service.impl.Mei;
-import net.jin.overwatch.service.impl.Reaper;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.*;
+import springfox.documentation.spring.web.plugins.*;
+import springfox.documentation.swagger2.annotations.*;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -45,7 +35,7 @@ public class JinAlphaApplication {
 		System.out.println();
 
 		//Print "Hello World"
-		System.out.println("Hello World");
+		System.out.println("Hello YoonSeo, I Love forever");
 
 		// null value input test
 		System.out.println();
@@ -55,27 +45,7 @@ public class JinAlphaApplication {
 		JavaHungry(s);
 		System.out.println();
 
-		// 다형성 테스트(OverWatch) : 입출력 처리부		
-		OverWatch overWatch; // 인터페이스 객체 선언
-		System.out.println("플레이할 캐릭터 번호 선택(1. 메이, 2. 리퍼, 3. 맥크리)");
-		Scanner scanner = new Scanner(System.in); // 스캐너 객체
-		int inputData = scanner.nextInt();
-		if (inputData == 1) {
-			overWatch = new Mei(); // 업캐스팅
-		} else if (inputData == 2) {
-			overWatch = new Reaper(); // 업캐스팅
-		} else {
-			overWatch = new Mccree(); // 업캐스팅
-		}
-        
-		// 다형성 테스트(OverWatch):	선택한 조건에 따라서 부모 객체로 자식 메소드 사용(하나의 타입으로 다양한 결과를 얻어냄 / 다형성)
-		overWatch.name();
-		overWatch.lClick();
-		overWatch.rClick();
-		overWatch.shiftButton();
-		overWatch.eButton();
-		overWatch.qButton();
-		
+
 /*********************************** End of Test Code *******************************************/
 	}
 	
