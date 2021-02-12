79f48d8aab72d4444dd57dfad9f4521ee0b9ea40
nice0001-angelo <nice0001@gmail.com>
2021-02-12 05:51:09 -0500

    update annotation

--- src/main/java/net/jin/answer/service/impl/CreateAnswerServiceImpl.java
+++ src/main/java/net/jin/answer/service/impl/CreateAnswerServiceImpl.java
@@ -14,7 +14,11 @@ import net.jin.repository.AnswerRepository;
 import net.jin.repository.QuestionRepository;
 import net.jin.util.HttpSessionUtils;
 
-@Service
+@Service 
+//IoC컨테이너에 등록되어 관리되는 객체를 빈(Bean)이라고 합니다. 
+//New 생성자를 이용해 생성한 객체는 IoC컨테이너에 등록된 객체가 아니므로 빈이 아니다
+//빈(Bean) 등록방법 3가지
+//1. Application context.xml(root-context.xml)에 등록 (Bean id와 클래스 명, 변수 이름, 의존관계 등을 xml로 작성하는 방법) 
 public class CreateAnswerServiceImpl implements CreateAnsewerService {
 	
 	@Autowired
